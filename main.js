1ed083afbd61cdf46afe796f1e8c8911c3f59e6e
vasilzhigilei <vasil.zhigilei@gmail.com>
2019-11-24 12:17:59 +0100

    added map

--- main.js
+++ main.js
@@ -2,11 +2,27 @@ var nodemailer = require('nodemailer');
 var sys = require('systeminformation');
 var readline = require('readline-sync');
 
-var possible_limits = ['CPU_temp', 'MEM_free', ]
+// List of available system limits
+var possible_limits = { CPU_temp: 120, MEM_free: 1024 };
 
+// Initial welcome message
+process.stdout.write('===SYSTEM EMAIL ALERT SYSTEM===\n');
 
-console.log('===SYSTEM EMAIL ALERT SYSTEM===');
+// Write to console array of available limits
+process.stdout.write('Available limits: ');
+Object.keys(possible_limits).forEach(element => {
+    process.stdout.write(element + '  ');
+});
+process.stdout.write('\n\n');
 
-var limit = readline.question('CPU limit? ');
+var done = false;
+while (done == false) {
+    // Select limit
+    var limit = readline.question('Select a limit or enter DONE: ');
 
-console.log('CPU limit is ' + limit);
+    if (Object.keys(possible_limits).indexOf(limit) > -1) {
+        console.log(true);
+    }
+}
+
+console.log('CPU limit is - ' + limit);
